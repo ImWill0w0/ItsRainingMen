48e3c86984f1c1fb8d804fcec52c9bc0e0371256
ImWill0w0 <28235017+ImWill0w0@users.noreply.github.com>
2021-08-22 19:54:26 -0700

    initial commit

---
+++ code/ItsRainingMen.cs
@@ -0,0 +1,41 @@
+﻿
+using Sandbox;
+using Sandbox.UI.Construct;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+//
+// You don't need to put things in a namespace, but it doesn't hurt.
+//
+namespace itsrainingmen
+{
+	[Library("itsrainingmen", Title = "itsrainingmen")]
+	public partial class itsrainingmen : Sandbox.Game
+	{
+		public itsrainingmen()
+		{
+			if ( IsServer )
+			{
+				ConsoleSystem.Run("playsound cockroachmusic");
+			}
+
+			if ( IsClient )
+			{
+			}
+		}
+
+		/// <summary>
+		/// A client has joined the server. Make them a pawn to play with
+		/// </summary>
+		public override void ClientJoined( Client client )
+		{
+			base.ClientJoined( client );
+
+			var player = new MinimalPlayer();
+			client.Pawn = player;
+
+			player.Respawn();
+		}
+	}
+}
---
+++ code/StaticCamera.cs
@@ -0,0 +1,31 @@
+﻿using Sandbox;
+using System;
+
+namespace comfortcockroach
+{
+	public partial class StaticCamera : Camera
+	{
+		public override void Activated()
+		{
+			if (Local.Pawn is Player player)
+			{
+				Pos = player.Position;
+				Rot = player.Rotation;
+			}
+
+			base.Activated();
+		}
+
+		public override void Update()
+		{
+			if (Local.Pawn is Player player)
+			{
+				FieldOfView = 65f;
+				Pos = Pos.LerpTo(100f,0f);
+				Rot = player.Rotation;
+			}
+
+			Viewer = null;
+		}
+	}
+}
